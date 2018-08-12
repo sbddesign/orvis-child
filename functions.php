0353822e15558a233173c8fc2c08d00c73181120
Stephen DeLorme <stephen@d.elor.me>
2018-08-12 16:04:10 -0400

    Added margin to Gutenberg image blocks

--- functions.php
+++ functions.php
@@ -2,6 +2,11 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
 }
 
 update_option('jetpack_portfolio_posts_per_page', 18, true);
